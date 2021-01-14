23dbfb9e4546d57f4773f9f05083bfb6b29922ad
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2021-01-14 10:18:23 -0700

    small tweak

--- components/website-cms/s3.tf
+++ components/website-cms/s3.tf
@@ -10,8 +10,7 @@ resource "aws_s3_bucket" "website-asset-bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "arn"
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
