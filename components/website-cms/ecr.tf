a65fff8abb0153960eab3db6b9474825416d783a
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2021-01-21 13:40:50 -0700

    app image stuff

--- components/website-cms/ecr.tf
+++ components/website-cms/ecr.tf
@@ -1,7 +1,38 @@
 resource "aws_ecr_repository" "image-repository" {
-  name      = "cds-website-cms-repository"
+  name      = "cds-website/cms"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "policy" {
+  repository = aws_ecr_repository.image-repository.name
+
+  policy = <<EOF
+  {
+      "rules": [
+          {
+              "rulePriority": 1,
+              "description": "Keep last 20 images",
+              "selection": {
+                  "tagStatus": "tagged",
+                  "tagPrefixList": ["v"],
+                  "countType": "imageCountMoreThan",
+                  "countNumber": 20
+              },
+              "action": {
+                  "type": "expire"
+              }
+          }
+      ]
+  }
+  EOF
+  }
+
+output "ecr_repository_url" {
+  value = {
+    for repo in aws_ecr_repository.image-repository :
+    repo.name => repo.repository_url
   }
 }
--- components/website-cms/ecs.tf
+++ components/website-cms/ecs.tf
@@ -6,7 +6,7 @@ data "template_file" "cms_app" {
   template = file("./task-definitions/cms_app.json.tpl")
 
   vars = {
-    app_image      = var.app_image
+    image          = aws_ecr_repository.image-repository.repository_url
     fargate_cpu    = var.fargate_cpu
     fargate_memory = var.fargate_memory
     aws_region     = "ca-central-1"
