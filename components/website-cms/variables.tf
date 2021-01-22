db73cc72bc16da189cf30071fceb03c200307596
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2021-01-22 08:08:55 -0700

    remove unneccessary variables

--- components/website-cms/variables.tf
+++ components/website-cms/variables.tf
@@ -17,20 +17,6 @@ variable "fargate_memory" {
 }
 
 # RDS DB
-variable "rds_cluster_host" {
-  type = string
-}
-variable "rds_db_name" {
-  type = string
-}
-
-variable "rds_db_user" {
-  type = string
-}
-variable "rds_db_password" {
-  type = string
-}
-
 variable "rds_cluster_password" {
   type = string
 }
