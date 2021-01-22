ce3d9139c256fd1d8ff2aec2a2a073fd9b004262
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2021-01-22 11:54:02 -0700

    remove spaces

--- components/website-cms/iam.tf
+++ components/website-cms/iam.tf
@@ -2,20 +2,20 @@ resource "aws_iam_role" "ecs-task-execution-role" {
   name = "ecs_task_execution_role"
 
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "ecs.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
   tags = {
     tag-key = "tag-value"
