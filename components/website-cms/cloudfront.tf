55d7cf453a8fa945c78f601edef5908dcbd34e99
brdunfield <bethany.dunfield@cds-snc.ca>
2021-02-19 13:05:34 -0700

    add restrictions

--- components/website-cms/cloudfront.tf
+++ components/website-cms/cloudfront.tf
@@ -41,6 +41,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     compress               = true
   }
 
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
   viewer_certificate {
     cloudfront_default_certificate = true
     minimum_protocol_version = "TLSv1.2_2019"
