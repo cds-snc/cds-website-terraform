dbf4a6fbda89e9d72b3d7d0d387792d4f96ed807
Max Neuvians <max.neuvians@cds-snc.ca>
2021-09-08 10:49:06 -0400

    fix: db version

--- components/website-cms/rds.tf
+++ components/website-cms/rds.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "website-cms-database" {
   allocated_storage         = 20
   storage_type              = "gp2"
   engine                    = "postgres"
-  engine_version            = "10.15"
+  engine_version            = "10.17"
   identifier                = "strapi"
   instance_class            = "db.t3.micro"
   name                      = "strapi"
