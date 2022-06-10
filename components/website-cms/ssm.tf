de4fa363ca1a8b58af0e89340112dce76cf4ccbb
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-06-10 10:50:17 -0400

    chore: switch to securestring

--- components/website-cms/ssm.tf
+++ components/website-cms/ssm.tf
@@ -1,23 +1,23 @@
 resource "aws_ssm_parameter" "db_password" {
   name  = "db_password"
-  type  = "String"
+  type  = "SecureString"
   value = var.rds_cluster_password
 }
 
 resource "aws_ssm_parameter" "github_token" {
   name  = "github_token"
-  type  = "String"
+  type  = "SecureString"
   value = var.github_token
 }
 
 resource "aws_ssm_parameter" "aws_access_key_id" {
   name  = "aws_access_key_id"
-  type  = "String"
+  type  = "SecureString"
   value = var.strapi_aws_access_key_id
 }
 
 resource "aws_ssm_parameter" "aws_secret_access_key" {
   name  = "aws_access_key_id"
-  type  = "String"
+  type  = "SecureString"
   value = var.strapi_aws_secret_access_key
 }
