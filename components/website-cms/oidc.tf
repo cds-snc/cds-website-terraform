d2335f807a2fb1b754447050e6b80c0e9fd51cfd
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-06-10 08:12:45 -0400

    feat: rename iam file and add oidc

---
+++ components/website-cms/oidc.tf
@@ -0,0 +1,57 @@
+locals {
+  plan_name  = "gh_plan_role"
+  admin_name = "gh_admin_role"
+}
+
+
+module "oidc" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v2.0.1//gh_oidc_role"
+  billing_tag_key   = "CostCentre"
+  billing_tag_value = var.product_name
+  oidc_exists       = false
+  roles = [
+    {
+      name : local.admin_name,
+      repo_name : "cds-website-terraform"
+      claim : "ref:refs/heads/main"
+    },
+    {
+      name : local.plan_name,
+      repo_name : "cds-website-terraform"
+      claim : "*"
+    }
+  ]
+}
+
+
+## Policy required for running TF Plans ReadOnly is needed along with some other policies.
+data "aws_iam_policy" "readonly" {
+  name = "ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "readonly" {
+  role       = local.plan_name
+  policy_arn = data.aws_iam_policy.readonly.arn
+}
+
+## Gives the plan role access to all secrets in the repo this is needed since ReadOnly doesn't provide that access
+## This also gives the plan role the ability to the state bucket and the dynamodb lock table.
+module "attach_tf_plan_policy" {
+  source     = "github.com/cds-snc/terraform-modules?ref=v2.0.1//attach_tf_plan_policy"
+  account_id = var.account_id
+  role_name  = local.plan_name
+  # This needs to match the data in the root terragrunt.hcl
+  bucket_name       = "${var.product_name}-tf"
+  lock_table_name   = "terraform-state-lock-dynamo"
+  billing_tag_key   = "CostCentre"
+  billing_tag_value = var.product_name
+}
+
+data "aws_iam_policy" "admin" {
+  name = "AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = local.admin_name
+  policy_arn = data.aws_iam_policy.admin.arn
+}
--- components/website-cms/task-role.tf
+++ components/website-cms/task-role.tf
@@ -21,26 +21,6 @@ data "aws_iam_policy_document" "task_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "cloudwatch_policies" {
-  role       = aws_iam_role.task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_container_registery_policies" {
-  role       = aws_iam_role.task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_container_registery_policies" {
-  role       = aws_iam_role.task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_iam_role_policy_attachment" "ecs_container_rds_full_access" {
   role       = aws_iam_role.task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
