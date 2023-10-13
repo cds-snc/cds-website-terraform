7b57e98f3e82ab6fa7aa2e819ad26e7e45be577c
Pat Heard <patrick.heard@cds-snc.ca>
2023-10-13 11:34:26 -0400

    fix: add missing strapi A record (#46)
    Add the A `alias` record to the load balancer.
    
    Upgrade to the latest Terraform and Terragrunt so that an
    `import` block can be used to import the existing A record.

--- components/website-cms/hosted_zone.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_zone" "strapi" {
-  name    = var.domain_name
-  comment = "Hosted zone for ${var.domain_name}"
-
-  tags = {
-    (var.billing_tag_key) = var.billing_tag_value
-    Terraform             = true
-  }
-}
---
+++ components/website-cms/route53.tf
@@ -0,0 +1,26 @@
+resource "aws_route53_zone" "strapi" {
+  name    = var.domain_name
+  comment = "Hosted zone for ${var.domain_name}"
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_route53_record" "strapi_A" {
+  zone_id = aws_route53_zone.strapi.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_alb.cms-load-balancer.dns_name
+    zone_id                = aws_alb.cms-load-balancer.zone_id
+    evaluate_target_health = false
+  }
+}
+
+import {
+  to = aws_route53_record.strapi_A
+  id = "${aws_route53_zone.strapi.zone_id}_${var.domain_name}_A"
+}
