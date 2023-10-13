311d15d15975441863c8c69844571001ac608f92
Pat Heard <patrick.heard@cds-snc.ca>
2023-10-13 14:11:46 +0000

    fix: fmt and add missing tags

--- components/website-cms/alb.tf
+++ components/website-cms/alb.tf
@@ -5,6 +5,14 @@ resource "aws_alb" "cms-load-balancer" {
   subnets         = aws_subnet.website-cms-public.*.id
   security_groups = [aws_security_group.website-cms-lb.id]
   internal        = false #tfsec:ignore:AWS005
+
+  drop_invalid_header_fields = true
+  enable_deletion_protection = true
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terrafrom             = true
+  }
 }
 
 resource "aws_alb_target_group" "app" {
@@ -31,10 +39,15 @@ resource "aws_alb_listener" "front_end" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-0-2021-06"
-  certificate_arn   = aws_acm_certificate_validation.strapi.certificate_arn 
+  certificate_arn   = aws_acm_certificate_validation.strapi.certificate_arn
 
   default_action {
     target_group_arn = aws_alb_target_group.app.id
     type             = "forward"
+  }
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terrafrom             = true
   }
 }
