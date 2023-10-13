37c6f5bc71460b9761344434ffbfe4c48ae69d13
Pat Heard <patrick.heard@cds-snc.ca>
2023-10-13 11:04:50 -0400

    fix: add missing strapi A record
    Add the A `alias` record to the load balancer.

---
+++ components/website-cms/route53.tf
@@ -0,0 +1,21 @@
+resource "aws_route53_zone" "strapi" {
+  name    = var.domain_name
+  comment = "Hosted zone for ${var.domain_name}"
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_route53_record" "strapi_A" {
+  zone_id = aws_route53_zone.strapi.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.cms-load-balancer.dns_name
+    zone_id                = aws_lb.cms-load-balancer.zone_id
+    evaluate_target_health = false
+  }
+}
